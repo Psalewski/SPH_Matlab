7ca6b9eadb1581e865fc4484e5324d474bb73301
Pascal Salewski <Pascal.Salew@gmail.com>
2021-12-10 09:44:28 +0100

    initial commit

---
+++ Glaettungfunktion_Darstellung.m
@@ -0,0 +1,39 @@
+%
+% Showing effects of the smoothening kernel 
+%
+clear all
+
+spaceing = 0.01;    %Verteilung der Partikel 
+h = 0.001;          %smoothining length;
+k= 1;
+hk = h*k;           %Einflussradius
+
+width = 1;
+height =width;
+Partikel = (width/spaceing +1) * (width/spaceing +1);
+Posx = zeros(Partikel);
+Posy = zeros(Partikel);
+x=spaceing;
+y=spaceing;
+for i = 1:Partikel
+
+
+    Posx(i) = x;
+    Posy(i) = y;
+
+    if x>=width
+        x=spaceing;
+        y = y + spaceing;
+    end
+    if x<width
+        x=x + spaceing;
+     
+    end
+
+
+end
+
+
+function y= gauss1d(glaet, dr)
+    y = (1/(pi *sqrt(glaet)))*  exp(-((dr*dr)/(glaet*glaet)));
+end
